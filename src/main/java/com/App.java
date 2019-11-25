7ad9c2f221e47cb7e69b2bb91d1e893e364824c6
teus_polec <teus.polec@gmail.com>
2019-11-25 22:45:44 +0100

    Update

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -3,19 +3,21 @@ package com;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+
 import java.net.URI;
 
-public class Main {
+public class App {
+
   protected static final String BASE_URI = "http://0.0.0.0:8123/";
+
+  protected static HttpServer startServer() {
+    final ResourceConfig rc = new ResourceConfig().packages("com");
+
+    return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+  }
 
   public static void main(String[] args) {
     startServer();
     System.out.println(String.format("Endpoint dostepny na %sstatus", BASE_URI));
   }
-  
-  protected static HttpServer startServer() {
-    final ResourceConfig rc = new ResourceConfig().packages("com");
-    return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-  }
 }
-
